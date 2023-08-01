1cb82b9bf4308cdffbeb05e15278baf89a978484
xuanthu2002 <tranxuanthu3721@gmail.com>
2023-08-02 00:27:07 +0700

    '.'

--- Kiem tra 1/bai toan tim kiem.cpp
+++ Kiem tra 1/bai toan tim kiem.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
-#include <utility>
-#include <algorithm>
 
 using namespace std;
 
-bool cmp(pair<int, int> a, pair<int, int> b) {
-	return a.second > b.second;
-}
-
 int main() {
-	int n;
-	cin >> n;
-	pair<int, int> p[n];
-	for(int i = 0; i < n; i++) cin >> p[i].first >> p[i].second;
-	sort(p, p + n, cmp);
-	bool slot[n] = {false};
-	long long kq = 0;
-	for(int i = 0; i < n; i++) {
-		for(int j = min(n, p[i].first) - 1; j >= 0; j--) {
-			if(slot[j] == false) {
-				kq += p[i].second;
-				slot[j] = true;
+	int t;
+	cin >> t;
+	while(t--) {
+		int n, s, k;
+		cin >> n >> s >> k;
+		int a[n + 1];
+		a[0] = 0;
+		for(int i = 1; i <= n; i++) {
+			int x;
+			cin >> x;
+			a[i] = a[i - 1] + x;
+		}
+		int kq = -1;
+		for(int i = 1; i <= s; i++) {
+			if(a[s] - a[i - 1] <= k) {
+				kq = i;
 				break;
 			}
 		}
+		cout << kq << endl;
 	}
-	cout << kq;
 	return 0;
 }
 
