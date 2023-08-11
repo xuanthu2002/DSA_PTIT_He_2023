4cbf2858b24f641542f7eb111b840fc17d518072
xuanthu2002 <tranxuanthu3721@gmail.com>
2023-08-11 16:31:16 +0700

    '11/8/2023'

--- DSA05047_chon o.cpp
+++ DSA05047_chon o.cpp
@@ -7,19 +7,19 @@ int main() {
 	cin >> t;
 	while(t--) {
 		int n, ans = -1e9, s = -1e9;
-        	cin >> n;
-        	int a[4][n + 1], f[n + 1][11] = {}, d[] = {0, 1, 2, 4, 5, 8, 9, 10};
-        	for(int i = 0; i < 4; i++) {
-        		for(int j = 1; j <= n; j++) {
-        			cin >> a[i][j];
-        			ans = max(ans, a[i][j]);
-				}
+        cin >> n;
+        int a[4][n + 1], f[n + 1][11] = {}, d[] = {0, 1, 2, 4, 5, 8, 9, 10};
+    	for(int i = 0; i < 4; i++) {
+    		for(int j = 1; j <= n; j++) {
+    			cin >> a[i][j];
+    			ans = max(ans, a[i][j]);
 			}
-			if(ans <= 0) {
-				cout << ans << endl;
-				continue;
-			}
-	        for(int j = 1; j <= n; j++) {
+		}
+		if(ans <= 0) {
+			cout << ans << endl;
+			continue;
+		}
+        for(int j = 1; j <= n; j++) {
 			for(int k = 0; k < 8; k++) {
 				for(int i = 0; i < 4; i++)
 					if(d[k] & (1 << i))
@@ -31,9 +31,8 @@ int main() {
 				f[j][d[k]] += x;
 			}
 		}
-	        for(int i = 0; i < 8; i++)
-			s = max(f[n][d[i]], s);
-	        cout << s << endl;
+        for(int i = 0; i < 8; i++) s = max(f[n][d[i]], s);
+        cout << s << endl;
 	}
 	return 0;
 }
